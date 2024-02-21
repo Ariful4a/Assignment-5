1451bffd24a7a2a2ade370df4b1080dea71c6d9d
Ariful4a <afkgamingno1@gmail.com>
2024-02-22 03:57:38 +0600

    all section complete

--- java-scripts/seat.js
+++ java-scripts/seat.js
@@ -11,12 +11,18 @@ for(const btn of allBtn){
     
         div.classList.add('gap-28');
 
+        const cartCount = getconvertValue('count');
+        document.getElementById('count').innerText = cartCount + 1;
+        const leftCartCount = getconvertValue('ll');
+        document.getElementById('ll').innerText = leftCartCount - 1;
+
+
         const p = document.createElement('p');
         const p2 = document.createElement('p');
         const p3 = document.createElement('p');
 
-        p.innerText = ('A1');
-        p2.innerText = ('Econome');
+        p.innerText = ('C1');
+        p2.innerText = ('Economoy');
         p3.innerText =(pric);
 
         div.appendChild(p);
@@ -25,8 +31,39 @@ for(const btn of allBtn){
 
         divcontainer.appendChild(div);
         updateTotalCost(pric);
+        updateGrandTotal();
     });
 }
+
+function updateGrandTotal(status){
+    const totalCost = getconvertValue('total-cost');
+    if(status == undefined){
+        document.getElementById('grand-total').innerText = totalCost;
+
+    }
+    else{
+        const couponCode = document.getElementById
+        ('coupon-code').value;
+       
+        if(couponCode == 'NEW15'){
+            const discounted = totalCost * 0.15;
+            document.getElementById('grand-total').innerText = totalCost - discounted;
+        }
+        else{
+            alert('Please enter valid coupon code');
+        }
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+
 
 function updateTotalCost(value){
     const totalCost = getconvertValue('total-cost');
@@ -36,9 +73,23 @@ function updateTotalCost(value){
 
 
 
-
 function getconvertValue (id){
     const price = document.getElementById(id).innerText;
     const convertPrice = parseInt(price);
     return convertPrice;
+}
+
+function changeColor(){
+    const greenColor = document.getElementById('make-color');
+    greenColor.classList.add('bg-green-500')
+}
+
+
+
+function next(){
+    const firstItem = document.getElementById('all-item');
+    firstItem.classList.add('hidden');
+
+    const nextItem = document.getElementById('next-section');
+    nextItem.classList.remove('hidden');
 }
